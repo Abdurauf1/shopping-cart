1ff14745d42f70babe8d25536770dc472fd84a32
Abdurauf <aabdurauf004@gmail.com>
2023-11-02 16:48:25 +0500

    refactor

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,47 @@
-const data: string = "../data.json";
+import { Product } from "./types.js";
+import { products } from "./data.js";
+
 const cartItemCountElem = document.querySelector(".cart-item-count") as HTMLSpanElement;
+const cart: Product[] = [];
 
-let cartItemCount = 0;
+let total: number = 0;
 
-cartItemCountElem.textContent = String(cartItemCount);
+cartItemCountElem.textContent = total.toString();
 
-const getData = (): void => {
-    fetch(data)
-        .then((res: Response) => {
-            if (!res.ok) {
-                console.log("Network response was not ok");
-            }
-            return res.json();
-        })
-        .then((data) => {
-            displayData(data.products);
-        })
-        .catch((err: Error) => {
-            console.log("Error: ", err);
-        })
+const addToCart = (product: Product): void => {
+    const existProd = cart.filter((prod: Product) => prod.name === product.name)
+    if (existProd) {
+        product.quantity++
+    } else {
+        cart.push(product)
+        total++
+        cartItemCountElem.textContent = total.toString()
+    }
 }
 
-const displayData = (data: any): void => {
+const openCart = (): void => {
+    console.log(cart);
+}
+
+const displayData = (data: Product[]): void => {
     const content = document.querySelector(".content") as HTMLDivElement;
     content.innerHTML = `
-        ${data.map((product: any) => (
-        `
-            <div class="product border text-center">
-                <img src="${product.img}" class="w-100" />
-                <h1>${product.name}</h1>
-                <p>${product.description}</p>
-                <button class="btn btn-primary">Add to cart</button>
+        ${data.map((product: Product) => {
+        const { image, name, price } = product;
+        return (
+            `
+            <div class="w-25 border text-center pb-3">
+                <div class="product-img">
+                    <img src="${image}" class="w-100" />
+                </div>
+                <h1 class="fs-2">${name}</h1>
+                <p class="fs-5 text-secondary">$${price}</p>
+                <button class="py-2 btn btn-success w-75" onClick={addToCart(${JSON.stringify(product)})}>Add to cart</button>
             </div>
         `
-    ))}
+        )
+    })}
     `;
 }
 
-getData();
-
-const addToCart = (): void => {
-
-}
+displayData(products)
