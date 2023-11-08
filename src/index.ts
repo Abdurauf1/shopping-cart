c74ee6ec94262a1b77b0cbe6cce059eae94c8015
Abdurauf <aabdurauf004@gmail.com>
2023-11-09 01:25:43 +0500

    refactor

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,60 @@
-import { Product } from "./types.js";
-import { products } from "./data.js";
+const products: Product[] = [
+    {
+        name: "Airpods",
+        price: 69,
+        image: "../images/airpods.png",
+        quantity: 0
+    },
+    {
+        name: "Earbuds",
+        price: 35,
+        image: "../images/earbuds.png",
+        quantity: 0
+    },
+    {
+        name: "iPhone Case",
+        price: 49,
+        image: "../images/iphone_case.png",
+        quantity: 0
+    },
+    {
+        name: "iPhone 15",
+        price: 899,
+        image: "../images/iphone.png",
+        quantity: 0
+    },
+    {
+        name: "Poco",
+        price: 129,
+        image: "../images/poco.png",
+        quantity: 0
+    },
+    {
+        name: "Redmi",
+        price: 280,
+        image: "../images/redmi.png",
+        quantity: 0
+    },
+    {
+        name: "Samsung",
+        price: 75,
+        image: "../images/samsung.png",
+        quantity: 0
+    },
+    {
+        name: "USB",
+        price: 25,
+        image: "../images/usb.png",
+        quantity: 0
+    },
+]
+
+interface Product {
+    name: string,
+    price: number,
+    image: string,
+    quantity: number
+}
 
 const cartItemCountElem = document.querySelector(".cart-item-count") as HTMLSpanElement;
 const cart: Product[] = [];
@@ -9,13 +64,13 @@ let total: number = 0;
 cartItemCountElem.textContent = total.toString();
 
 const addToCart = (product: Product): void => {
-    const existProd = cart.filter((prod: Product) => prod.name === product.name)
+    const existProd = cart.filter((prod: Product) => prod.name === product.name);
     if (existProd) {
-        product.quantity++
+        product.quantity++;
     } else {
-        cart.push(product)
-        total++
-        cartItemCountElem.textContent = total.toString()
+        cart.push(product);
+        total++;
+        cartItemCountElem.textContent = total.toString();
     }
 }
 
@@ -25,22 +80,23 @@ const openCart = (): void => {
 
 const displayData = (data: Product[]): void => {
     const content = document.querySelector(".content") as HTMLDivElement;
+
     content.innerHTML = `
         ${data.map((product: Product) => {
         const { image, name, price } = product;
         return (
             `
             <div class="w-25 border text-center pb-3">
-                <div class="product-img">
+                <div class="d-flex align-items-center product-img">
                     <img src="${image}" class="w-100" />
                 </div>
                 <h1 class="fs-2">${name}</h1>
                 <p class="fs-5 text-secondary">$${price}</p>
-                <button class="py-2 btn btn-success w-75" onClick={addToCart(${JSON.stringify(product)})}>Add to cart</button>
+                <button class="rounded-0 py-2 btn btn-success w-75" onClick="${addToCart(product)}">Add to cart</button>
             </div>
         `
         )
-    })}
+    }).join("")}
     `;
 }
 
