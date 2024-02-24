ce59539001e3b45e686aade1b2a7db43719b8710
Abdurauf <aabdurauf004@gmail.com>
2024-02-24 17:58:42 +0500

    feat: add fetch function

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,35 @@
 "use strict";
-console.log("hello");
-console.log("hello");
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+let data = [];
+let cart = [];
+const fetchProducts = () => __awaiter(void 0, void 0, void 0, function* () {
+    const response = yield fetch("https://fakestoreapi.com/products/");
+    data = yield response.json();
+    displayCards();
+    console.log(data);
+});
+const displayCards = () => {
+    const cardsWrapper = document.querySelector(".cards-wrapper");
+    data.forEach((product) => {
+        cardsWrapper.innerHTML += `
+            <div class="card">
+                <img src="${product.image}" alt="product-image" />
+                <p>${product.title}</p>
+                <p>$${product.price}</p>
+                <button onClick="addToCart()">Add To Cart</button>
+            </div>
+            `;
+    });
+};
+fetchProducts();
+const addToCart = () => {
+    console.log(cart);
+};
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,30 @@
-console.log("hello");
-console.log("hello");
+let data: any = []
+let cart: any = []
+
+const fetchProducts = async () => {
+    const response = await fetch("https://fakestoreapi.com/products/");
+    data = await response.json()
+    displayCards()
+    console.log(data);
+}
+
+const displayCards = (): void => {
+    const cardsWrapper = document.querySelector(".cards-wrapper") as HTMLElement
+
+    data.forEach((product: any) => {
+        cardsWrapper.innerHTML += `
+            <div class="card">
+                <img src="${product.image}" alt="product-image" />
+                <p>${product.title}</p>
+                <p>$${product.price}</p>
+                <button onClick="addToCart()">Add To Cart</button>
+            </div>
+            `;
+    })
+}
+
+fetchProducts()
+
+const addToCart = (): void => {
+    console.log(cart);
+}
